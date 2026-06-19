fc648b35ee4451b6ddd718720e776774f36fc08e
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-06-18 23:13:43 -0300

    comentários de código

---
+++ aula02/comentarios.js
@@ -0,0 +1,15 @@
+// Escrevendo um comentário
+
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1')
+console.log('Linha 2')
+
+/*
+Um bloco de código
+Linha de comentário
+*/
+
+console.log('Linha 3')
+console.log('Linha 4')
+
+// Qualquer comentário é ignorado pela engine, motor
