217249498b705b0c2eb258a81bb1d8dbbfe4987d
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-06-23 12:11:19 -0300

    let vs VAR - primeiro diferença

---
+++ aula06/index.js
@@ -0,0 +1,6 @@
+// ECMAScript (2015) (ES6)
+var nome = 'Wellison';
+var nome = 'Cavalcante';
+// let causaria erro.
+
+console.log(nome);
--- exercicio01/index.js
+++ exercicio01/index.js
@@ -6,7 +6,7 @@ const alturaEmMetros = 1.73;
 let IMC = peso / (alturaEmMetros**2);
 const anoDeNascimento = 2026-idade;
 
-console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`)
-console.log(`tem ${alturaEmMetros} de altura e seu IMC é de: ${IMC}`)
-console.log(`${nome} ${sobrenome} nasceu em: ${anoDeNascimento}`)
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmMetros} de altura e seu IMC é de: ${IMC}`);
+console.log(`${nome} ${sobrenome} nasceu em: ${anoDeNascimento}`);
 
