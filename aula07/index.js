4d644d316fb6b0eab0707ceb01294601236e3a55
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-01 23:04:16 -0300

    tipos de dados primitivos

---
+++ aula07/index.js
@@ -0,0 +1,20 @@
+// String, number, undefined, null, boolean, symbol
+const nome1 = 'Wellison'; // String
+const nome2 = "Wellison"; // String
+const nome3 = `Wellison`; // String
+const num1 = 10; // Number
+const num2 = 10.012; // Number
+let nomeAluno; // Undefined == Não aponta para nenhum local na memória
+let sobrenomeAluno = null; // Nulo == Não aponta para nenhum local na memória
+const aprovado = true; // Boolean
+const reprovado = false; // Boolean
+
+console.log(typeof num1, num1);
+console.log(typeof num2, num2);
+console.log(typeof nome1, nome1);
+console.log(typeof nome2, nome2);
+console.log(typeof nome3, nome3);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
+console.log(typeof reprovado, reprovado);
