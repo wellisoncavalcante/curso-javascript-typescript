3dfeec71a4fdf82ca34657516ab8b5d5673ae8eb
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-03 22:50:29 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ aula08/index.js
@@ -0,0 +1,23 @@
+/*
+    Operadores aritméticos
+    + (Adição e concatenação)
+    - (Subtração)
+    * (Multiplicação)
+    / (Divisão)
+    % (Resto da divisão)
+    ** (Exponenciação)
+    () ** * / % + - 
+
+    Incremento ++
+    Decremento = --
+
+    NaN - Not a Number.
+    ParseInt() - Converte uma string para um número inteiro.
+    ParseFloat() - Converte uma string para um número decimal.
+*/
+
+const num1 = 2;
+const num2 = Number('Wellison');
+
+console.log(num1 + num2);
+console.log(typeof num2);
