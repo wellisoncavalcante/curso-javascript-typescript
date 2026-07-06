8ed4a158cad21f9449c0808ee961dd0a6dd513b6
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-06 15:04:30 -0300

    Exercício com strings em JS

---
+++ aula11/ex/script.js
@@ -0,0 +1,10 @@
+const nome = prompt("Informe o seu nome completo: ");
+document.body.innerHTML += `Seu nome é: ${nome} <br />`;
+document.body.innerHTML += `Seu nome tem letras: ${nome.length} <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra a do seu nome? ${nome.search('a')} <br />`;
+document.body.innerHTML += `Qual o último índice da letra a do seu nome? ${nome.lastIndexOf('a')} <br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
+document.body.innerHTML += `As palavras do seu nome são:  ${nome.match(/[a-z]/gi)} <br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()} <br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()} <br />`;
