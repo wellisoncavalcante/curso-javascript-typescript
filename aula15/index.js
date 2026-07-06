5a652d8c8fb0727dedc71f35236ce4d4a9f8d16b
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-06 19:19:21 -0300

    Aprendendo um pouco sobre funções

---
+++ aula15/index.js
@@ -0,0 +1,27 @@
+/* function saudacao(nome) {
+    return `Bom dia ${nome}!`
+}
+
+const variavel = saudacao('Wellison');
+console.log(variavel);
+*/
+
+/* function media(VA1, VA2) {
+    return (VA1 + VA2) / 2
+}
+
+console.log(media(10, 9)); */
+
+/* function soma(x = 1, y= 1) {
+    const resultado = x + y;
+    return resultado
+}
+
+const resultado = soma(4, 2);
+console.log(resultado); */
+
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
