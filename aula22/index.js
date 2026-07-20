78a6ca09571ea65b52e2b5aa52c678a215c23628
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-20 18:49:53 -0300

    operadores lógicos em javascript

---
+++ aula22/index.js
@@ -0,0 +1,18 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar TRUE
+|| -> OR -> OU -> Apenas uma das expressões precisam ser verdadeira para retornar TRUE
+! -> NOT -> NÃO -> 
+*/
+
+// &&: true and true == TRUE.
+// &&: true and false == FALSE.
+// &&: false and true == FALSE.
+// &&: false and false == FALSE.
+
+// ||: true or true == TRUE
+// ||: true or false == TRUE
+// ||: false or true == TRUE
+// ||: false or false == FALSE
+
+console.log();
