2ebbbefd877267a1193efd9ace5063d07e3c6766
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-20 18:28:57 -0300

    operadores de comparação em javascript

---
+++ aula21/index.js
@@ -0,0 +1,15 @@
+/*
+
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+
+*/
+const comparacao = 10 <= 1;
+console.log(comparacao);
